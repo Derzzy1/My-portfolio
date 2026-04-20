bec4ebda82e6298fb72445f3a260a8c9e1ee6907
Ideraoluwa <ideraoluwac@gmail.com>
2026-04-20 18:13:29 +1100

    big

--- src/static/Navbar.jsx
+++ src/static/Navbar.jsx
@@ -3,25 +3,32 @@ import { Menu, X } from "lucide-react";
 import {Link} from 'react-router-dom';
 
 
-const Navbar = () => {
-return (
+function Navbar()  {
+const [isOpen, setIsOpen] = useState(false);
+
+
+  return (
     <nav className=" header">
       <div className=" nav">
 
         <img src="/images/lg1.jpeg" alt="" className="img"/>
-        <div className= "nav-links">
+        <div className={`nav-links ${isOpen ? "active" : ""}`}>
           <Link to="/" className="">
             Home
           </Link>
           <Link to="/about" className="">
             About
           </Link>
-          <Link to="/Projects" className="hi">
+          <Link to="/Projects" className="">
             Projects
           </Link>
-          <Link to="/contacts" className="hi">
+          <Link to="/contacts" className="">
             Contacts
           </Link>
+          </div>
+          <div className="hamburger"
+          onClick = {() => setIsOpen(!isOpen)}>
+             { isOpen ? <X size={28}/> : <Menu size={28} /> }
           </div>
         </div>
     </nav>
