b5f0d0d8fcf831cf25990872cd07eaf285a81d4e
Ideraoluwa <ideraoluwac@gmail.com>
2026-04-22 20:47:12 +1100

    eba

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
      <div className="fs1">
       <h1 className='n1'> Caleb </h1>
        <h1 className='n1'>Ideraoluwa </h1>
-       <h1> Frontend developer crafting responsive, high-performance web apps with React and modern JavaScript. </h1>
-       <h1>I don’t just build websites—I create experiences people enjoy using. </h1>
+       <h1 className='n2'> Frontend developer crafting responsive, high-performance web apps with React and modern JavaScript. </h1>
+       <h1 className='n2'>I don’t just build websites—I create experiences people enjoy using. </h1>
        <ul className='skills'>
         <li>Focused on clean UI, smooth animations, and real-world usability.</li>
         <li>Currently building projects and leveling up my frontend skills.</li>
