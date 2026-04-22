97490481b6c2348d070b40eaed7f6e8cb081a36d
Ideraoluwa <ideraoluwac@gmail.com>
2026-04-22 21:36:34 +1100

    kiuyt

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,19 +6,19 @@ const Projects = () => {
       <div className="free">
         <img src="/images/Grade.png" alt="" />
         <h1>A JS function that grades them according to their scores</h1>
-        <a href="grade-y2u1.vercel.app"className='hre'>View project</a>
+        <a href="grade-y2u1.vercel.app">View project</a>
       </div>
       <div className="bx"></div>
       <div className="free">
         <img src="/images/Meta.png" alt="" />
         <h1> Cloning Meta product and welfare page including the Header and Footer</h1>
-            <a href="meta-brown-chi.vercel.app"className='hre'>View project</a>
+            <a href="meta-brown-chi.vercel.app">View project</a>
       </div>
       <div className="bx"></div>
       <div className="free">
         <img src="/images/port.png" alt="" />
         <h1>My personal portfolio</h1>
-            <a href="derzzy.vercel.app"className='hre'>View project</a>
+            <a href="derzzy.vercel.app">View project</a>
       </div>
 
   
